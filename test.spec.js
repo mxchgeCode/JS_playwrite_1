edd616d5061d46da2e64337034ef4fdcb9e60816
mtx <dadbarn@gmail.com>
2025-03-11 17:49:05 +0300

    fix browser config

--- test.spec.js
+++ test.spec.js
@@ -1,17 +1,11 @@
 const {test, expect} = require('@playwright/test');
-const {chromium} = require('playwright');
-
 test('click test', async ({page}) => {
-
-    const browser = await chromium.launch({headless: false});
     await page.goto('http://uitestingplayground.com/click');
     const button = page.locator("#badButton");
     await expect(button).toHaveCSS('background-color', 'rgb(0, 123, 255)')
     await button.click();
     await expect(button).toHaveCSS('background-color', 'rgb(33, 136, 56)');
-    await page.waitForTimeout(3000); // Подождать 3 секунды
-    await browser.close(); // Закрыть браузер
-
+    await page.waitForTimeout(3000);
 });
 
 
