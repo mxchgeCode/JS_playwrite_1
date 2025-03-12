61ad4b1d1645def9598e6f27541419b23c1d48bd
mtx <dadbarn@gmail.com>
2025-03-12 15:32:39 +0300

    add more tests

--- test.spec.js
+++ test.spec.js
@@ -1,4 +1,13 @@
 const {test, expect} = require('@playwright/test');
+test('checkbox and radio', async ({page}) => {
+    await page.goto('https://jqueryui.com/resources/demos/checkboxradio/default.html');
+    await page.locator("[for=radio-1]").click();
+    await page.locator("[for=checkbox-4]").click();
+    await page.locator("[for=checkbox-nested-3]").click();
+    await page.locator("[for=checkbox-nested-4]").click();
+});
+
+
 test('click test', async ({page}) => {
     await page.goto('http://uitestingplayground.com/click');
     const button = page.locator("#badButton");
@@ -9,3 +18,49 @@ test('click test', async ({page}) => {
 });
 
 
+test('fill data test', async ({page}) => {
+    const login = 'john'
+    await page.goto('http://uitestingplayground.com/sampleapp');
+    await page.fill(`input[name="UserName"]`, login);
+    await page.fill('input[name="Password"]', 'pwd');
+    await page.locator('[id="login"]').click();
+    await page.waitForTimeout(3000);
+    await expect(page.locator("#loginstatus")).toHaveText(`Welcome, ${login}!`);
+})
+
+test('get image caption', async ({page}) => {
+    await page.goto('http://the-internet.herokuapp.com/hovers');
+    await page.locator(".figure").nth(1).hover();
+    await expect(page.locator('a[href="/users/2"]')).toBeVisible();
+})
+
+
+test('checkboxes', async ({page}) => {
+    await page.goto('http://the-internet.herokuapp.com/checkboxes');
+    const form = page.locator("#checkboxes input")
+    const cb1 = form.nth(0);
+    const cb2 = form.nth(1);
+
+    await cb1.check();
+    await cb2.uncheck();
+
+    await expect(cb1).toBeChecked()
+    await expect(cb2).not.toBeChecked()
+})
+
+
+test('screen_shot', async ({page}) => {
+    const screenshotPath = 'site.png'
+    const containerScreenshotPath = 'container.png'
+    await page.goto('http://uitestingplayground.com/sampleapp');
+    await page.locator("body").screenshot({path: screenshotPath});
+    await page.waitForTimeout(500);
+    await page.locator('.container').first().screenshot({path: containerScreenshotPath});
+    await page.waitForTimeout(500);
+    const fs = require('fs');
+    const bodyFileExists = fs.existsSync(screenshotPath);
+    const containerFileExists = fs.existsSync(containerScreenshotPath);
+    expect(bodyFileExists, `Файл ${screenshotPath} не был создан`).toBe(true);
+    expect(containerFileExists, `Файл ${containerScreenshotPath} не был создан`).toBe(true);
+
+})
